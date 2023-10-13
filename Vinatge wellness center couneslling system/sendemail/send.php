7b732fa4def465332d2fcff3f0e47941ee36a64e
Riley matewere <81653537+riley071@users.noreply.github.com>
2023-10-13 16:35:58 +0200

    Update send.php

--- Vinatge wellness center couneslling system/sendemail/send.php
+++ Vinatge wellness center couneslling system/sendemail/send.php
@@ -12,7 +12,7 @@ if(isset($_POST["send"])){
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'emzomatewere@gmail.com';   
+    $mail->Username = '';   
     $mail->Password = 'jihhxcrfsoacepnl';
     $mail->SMTPSecure = 'ssl';
     $mail->Port = 465;
